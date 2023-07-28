7ed6ec8c5b47bf5ace9b419d768ef68916c1656c
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-28 15:11:29 +0100

    handele cache probleme

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,28 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import { Header, Clients } from "./components";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache()
+  cache
 })
 
 function App() {
